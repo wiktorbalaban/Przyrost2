e6604a82e840760f059223e30b3119b4baa4e858
Wiktor <wiktorbalaban@gmail.com>
2017-11-18 19:39:03 +0100

    Próba obsługi serwera poprzez hibernate

---
+++ src/main/java/hibernate/Test/Manager.java
@@ -0,0 +1,48 @@
+package hibernate.Test;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+class Manager {
+
+    public static void main(String[] args) {
+
+        System.out.println("TestManagerStart");
+
+        EntityManager entityManager = null;
+
+        EntityManagerFactory entityManagerFactory = null;
+
+        try {
+            //taka nazwa jak w persistence.xml
+            entityManagerFactory = Persistence.createEntityManagerFactory("hibernate-dynamic");
+            entityManager = entityManagerFactory.createEntityManager();
+            entityManager.getTransaction().begin();
+
+            TestTable table = new TestTable();
+            table.setTableName("Pierwsza");
+            table.setColumnCount(5);
+
+            entityManager.persist(table);
+
+            TestTable tableFromBase = entityManager.find(TestTable.class, table.getId());
+
+            System.out.println("TestTable " + tableFromBase.getId() + " " + tableFromBase.getTableName() +
+                    " have " + tableFromBase.getColumnCount() + "columns");
+
+            //zakoncz transakcje
+            entityManager.getTransaction().commit();
+
+            System.out.println("Done");
+
+            entityManager.close();
+
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+        } finally {
+            if (entityManager != null) entityManagerFactory.close();
+        }
+    }
+
+}
---
+++ src/main/java/hibernate/Test/TestTable.java
@@ -0,0 +1,48 @@
+package hibernate.Test;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TestTable", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"table_name"})
+})
+class TestTable {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "table_name")
+    private String tableName;
+
+    @Column(name = "cloumn_count")
+    private int columnCount;
+
+    public TestTable() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTableName() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName = tableName;
+    }
+
+    public int getColumnCount() {
+        return columnCount;
+    }
+
+    public void setColumnCount(int columnCount) {
+        this.columnCount = columnCount;
+    }
+}
