562558c7ef6a04851b539d5b23a892f5461e0b1d
Wiktor Bałaban <wiktorbalaban@gmail.com>
2017-11-23 09:43:27 +0100

    Zapytania 5 gotowe, z relacją many to many też.

--- src/main/java/Querys.java
+++ src/main/java/Querys.java
@@ -1,3 +1,5 @@
+import hibernate.model.FightingSchool;
+import hibernate.model.Technique;
 import hibernate.model.Warrior;
 
 import javax.persistence.EntityManager;
@@ -5,6 +7,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class Querys {
@@ -26,8 +29,10 @@ public class Querys {
             //rozpocznij transakcje
             entityManager.getTransaction().begin();
 
-            List<Warrior> warriors=getWarriorsQuery(entityManager);
-            //getWarriorsWhoHasThisTechniqueQuery(entityManager,"Kamehame-Ha");
+            //List<Warrior> warriors=getWarriorsQuery(entityManager);
+            List<Warrior> warriors =getWarriorsWhoHasThisTechniqueQuery(entityManager,"Kamehame-Ha");
+            //List<FightingSchool> fightingSchools=getFightingSchoolsQuery(entityManager);
+            //List<Collection> techniques=getWarriorsTechniquesQuery(entityManager);
 
             //zakoncz transakcje
             entityManager.getTransaction().commit();
@@ -48,10 +53,22 @@ public class Querys {
         TypedQuery<Warrior> query =entityManager.createQuery("SELECT k FROM Warrior k", Warrior.class);
         return query.getResultList();
     }
-//    static List<Warrior> getWarriorsWhoHasThisTechniqueQuery(EntityManager entityManager,String techniqueName){
-//        String queryString="select w from Technique as t inner join Warrior_technique as wt on t.name = :techniquename"
-//        +" and wt.techniqueId = t.id inner join Warrior as w on w.id = wt.wariorId";
-//        TypedQuery<Warrior> query =entityManager.createQuery(queryString, Warrior.class);
-//        return query.setParameter("techniquename",techniqueName).getResultList();
-//    }
+    static List<FightingSchool> getFightingSchoolsQuery(EntityManager entityManager){
+        TypedQuery<FightingSchool> query =entityManager.createQuery("SELECT k FROM FightingSchool k", FightingSchool.class);
+        return query.getResultList();
+    }
+    static List<Warrior> getWarriorsWitchSurnameQuery(EntityManager entityManager,String surname){
+        TypedQuery<Warrior> query =entityManager.createQuery("SELECT k FROM Warrior k where k.surname = :surname", Warrior.class);
+        return query.setParameter("surname",surname).getResultList();
+    }
+    static List<Warrior> getWarriorsWhoHasThisTechniqueQuery(EntityManager entityManager,String techniqueName){
+        String queryString="select w from Warrior as w inner join w.techniques as t where t.name = :techniquename";
+        TypedQuery<Warrior> query =entityManager.createQuery(queryString, Warrior.class);
+        return query.setParameter("techniquename",techniqueName).getResultList();
+    }
+    static List<Collection> getWarriorsTechniquesQuery(EntityManager entityManager){
+        String queryString="select w.techniques from Warrior w";
+        TypedQuery<Collection> query =entityManager.createQuery(queryString, Collection.class);
+        return query.getResultList();
+    }
 }
